40845a4b633fbe5685c52cb7214197d0d8b9674e
mefeyilmaztekin <60896455+mefeyilmaztekin@users.noreply.github.com>
2021-03-02 01:07:28 +0300

    Update checkprocess.vb
    Code changed

--- checkprocess.vb
+++ checkprocess.vb
@@ -1,34 +1,12 @@
-# Tested on Visual Studio 2017/2019 
-
-Create new Private Sub
-
 Private Sub processchecker()
         Dim p() As Process ' p is process
         p = Process.GetProcessesByName("processname") ' process's name
         If p.Count > 0 Then ' If Process is running
             ' Process is running
-            
+            MsgBox("ProcessName.exe dedected")
         Else
             ' Process is not running
-            
+            MsgBox("Please Open ProcessName.exe")
         End If
  
- For Example (Streamer Mode | Obs Studio 64) 
- 
- Private Sub streamerobs()
-        Dim p() As Process
-        p = Process.GetProcessesByName("obs64")
-        If p.Count > 0 Then
-            ' Process is running
-            MsgBox("Streamer Mode ON", MsgBoxStyle.Information, "")
-        Else
-            ' Process is not running    
-        End If
-        
-  And Type private sub's name 
-  
-  Example :
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-      streamerobs()
-        End Sub
-        
+
